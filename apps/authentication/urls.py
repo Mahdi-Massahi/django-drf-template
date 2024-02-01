8d707e824b86fd48b789746896944e3e6cafdb03
Mahdi-Massahi <Mahdi.Massahi@gmail.com>
2024-02-01 16:11:24 +0330

    fix: user auth endpoints

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from .views import UserViewSet, CurrentUserView
+from .views import UserViewSet
 
 router = routers.DefaultRouter()
 
@@ -13,7 +13,6 @@ router.register(r"user", UserViewSet)
 
 
 urlpatterns = [
-    path("current_user/", CurrentUserView.as_view(), name="current_user"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", include(router.urls)),
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,26 +1,12 @@
-from rest_framework import viewsets, views
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import viewsets, mixins
 
 from .models import User
 from .serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-class CurrentUserView(views.APIView):
-    """
-    get the current user info based on the token
-    """
-
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        user = request.user
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
